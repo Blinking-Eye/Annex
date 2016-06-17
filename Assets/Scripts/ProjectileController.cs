b4e851e41d4aac9262c5722f037904edda78c13e
Ben Collins <sstben@gmail.com>
2016-06-17 02:02:43 -0500

    added shooting

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -1,36 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEditor;
 
 public class ProjectileController : MonoBehaviour
 {
     // Object that instantiated projectile
     public ShootingController Player;
-    private Rigidbody2D _rb;
+    public float Speed;
 
 	// Use this for initialization
     private void Start()
     {
         Destroy(gameObject, 5.0f); // Destroy after 5 seconds
-
-        _rb = GetComponent<Rigidbody2D>();
-
-        // Set damage based on power of shot
-        if (Player.Power >= 70.0f)
-            Damage = 30.0f;
-        else
-            Damage = 10.0f;
+        
+        Damage = 10.0f;
 
         // Say whether enemy shot or friendly shot
         if (Player.CompareTag("Player"))
-            gameObject.tag = "PlayerProjectile";
+        {
+            gameObject.tag = "PlayerLemon";
+        }
         else
-            gameObject.tag = "EnemyProjectile";
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-        transform.LookAt(transform.position - (Vector3)_rb.velocity.normalized);
+            gameObject.tag = "EnemyLemon";
     }
 
     void OnCollisionEnter2D(Collision2D collision2D)
